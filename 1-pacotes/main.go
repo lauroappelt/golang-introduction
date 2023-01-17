4d2e7cf4b76c5983f7ba22d6c7de76f24ac769ad
lauro <lauro@solis.com.br>
2023-01-16 21:44:03 -0300

    Aula 2 - instalando modulos externos

--- 1-pacotes/main.go
+++ 1-pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main () {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste@gmail.com")
+	fmt.Println(erro)
 }
